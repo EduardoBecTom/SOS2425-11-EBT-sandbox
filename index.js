985376e98019d161c5e206188a7542fdf0502f3c
Eduardo Becerra Tome <eduardo4b@gmail.com>
2025-02-21 12:06:37 +0100

    Carita cool

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!');
+const express = require('express');
+const cool = require('cool-ascii-faces');
+const app = express();
+const PORT = 16078;
+
+app.get('/', (request, response) =>{
+    response.send('Hello from the server!');
+}
+);
+
+app.get('/cool', (request, response) =>{
+    response.send(cool());
+}
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
